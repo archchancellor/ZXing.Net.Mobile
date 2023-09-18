8d5d8212316a456e381ceb773dd03b0b3bb502fa
detritus <sinan.guven@idesse.com.tr>
2023-09-18 21:00:41 +0300

    build: fix relative paths in build script

--- artifacts/_build.ps1
+++ artifacts/_build.ps1
@@ -1,4 +1,4 @@
 $pkgVer = "3.1.0-beta-idesse"
-New-Item -ItemType Directory -Force -Path .\artifacts
-msbuild /t:Pack /p:Configuration=Release /p:PackageOutputPath=..\artifacts /p:PackageVersion=$pkgVer /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg .\ZXing.Net.Mobile\ZXing.Net.Mobile.csproj
-msbuild /t:Pack /p:Configuration=Release /p:PackageOutputPath=..\artifacts /p:PackageVersion=$pkgVer /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg .\ZXing.Net.Mobile.Forms\ZXing.Net.Mobile.Forms.csproj
+New-Item -ItemType Directory -Force -Path .\nuget
+msbuild /t:Pack /p:Configuration=Release /p:PackageOutputPath=..\artifacts\nuget /p:PackageVersion=$pkgVer /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg ..\ZXing.Net.Mobile\ZXing.Net.Mobile.csproj
+msbuild /t:Pack /p:Configuration=Release /p:PackageOutputPath=..\artifacts\nuget /p:PackageVersion=$pkgVer /p:IncludeSymbols=true /p:SymbolPackageFormat=snupkg ..\ZXing.Net.Mobile.Forms\ZXing.Net.Mobile.Forms.csproj
